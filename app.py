77cae4e2d4fd5a570ccd98b642a8b82e846983b8
Miles <mpluders@gmail.com>
2016-08-18 16:39:23 -0700

    Changed port binding

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def home():
     return render_template('index.html',
                            dayparts=data_engine.get_caf_dayparts())
 
-port = int(os.environ.get("PORT", 5004))
-app.run(port=port)
+#port = int(os.environ.get("PORT", 5004))
+#app.run(port=port
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
